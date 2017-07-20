3681227cddd03d43be70f8035d990803c7d6361c
= <chase.terry87@gmail.com>
2017-07-20 11:07:27 -0500

    Added a bit more to the home page

--- application/views/home.php
+++ application/views/home.php
@@ -14,3 +14,37 @@
 		</div>
 	</div>
 </section>
+
+<br/>
+
+<div class="container">
+	<div class="columns">
+		<div class="column is-one-third center">
+			<img src="<?php echo $this->path('public/images/time.png'); ?>" width="120" height="120"/>
+			<br/>
+			<h2>Convenience</h2>
+			<p class="content">
+				Who has time to go to the gym? 
+				With our streaming service, you don't have to leave your living room! 
+			</p>
+		</div>
+		<div class="column is-one-third center">
+			<img src="<?php echo $this->path('public/images/weights.png'); ?>" width="120" height="120"/>
+			<br/>
+			<h2>Variety</h2>
+			<p class="content">
+				Tired of doing the same workouts every day?  Mix it up!
+				Choose from our huge selection of workouts when you sign up!
+			</p>
+		</div>
+		<div class="column is-one-third center">
+			<img src="<?php echo $this->path('public/images/reward.png'); ?>" width="120" height="120"/>
+			<br/>
+			<h2>Rewarding</h2>
+			<p class="content">
+				Keep track of your progress when you complete workouts.
+				Add friends to build accountability.
+			</p>
+		</div>
+	</div>
+</div>
