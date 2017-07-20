2d1e3442dd6636f697ee391d8273a4dbd38c6d35
= <chase.terry87@gmail.com>
2017-07-20 11:34:41 -0500

    Added a little more to the home page

--- application/views/home.php
+++ application/views/home.php
@@ -38,12 +38,41 @@
 			</p>
 		</div>
 		<div class="column is-one-third center">
+			<img src="<?php echo $this->path('public/images/intelligent.png'); ?>" width="120" height="120"/>
+			<br/>
+			<h2>Intelligent</h2>
+			<p class="content">
+				Keep track of your progress with detailed analytics.
+				Set your own goals, and we will help you reach them!
+			</p>
+		</div>
+	</div>
+	<div class="columns">
+		<div class="column is-one-third center">
+			<img src="<?php echo $this->path('public/images/social.png'); ?>" width="120" height="120"/>
+			<br/>
+			<h2>Social</h2>
+			<p class="content">
+				Add friends to build accountability.  Keep track of one another,
+				encourage one another, and you can even make new friends with our social portal!
+			</p>
+		</div>
+		<div class="column is-one-third center">
 			<img src="<?php echo $this->path('public/images/reward.png'); ?>" width="120" height="120"/>
 			<br/>
 			<h2>Rewarding</h2>
 			<p class="content">
-				Keep track of your progress when you complete workouts.
-				Add friends to build accountability.
+				Everyone can use a little bit of motivation when it comes to working out.
+				Our site rewards you with achievements when you accomplish something big!
+			</p>
+		</div>
+		<div class="column is-one-third center">
+			<img src="<?php echo $this->path('public/images/money.png'); ?>" width="120" height="120"/>
+			<br/>
+			<h2>Free!</h2>
+			<p class="content">
+				You get all of these amazing features for free!
+				We want you to be the best YOU without emptying your bank account!
 			</p>
 		</div>
 	</div>
