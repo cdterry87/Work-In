b320bfc9445e0f1a01a67886f06d0eda026c4da3
= <chase.terry87@gmail.com>
2017-07-26 10:51:12 -0500

    Adjusted the sidebar a bit

--- application/views/account/main.php
+++ application/views/account/main.php
@@ -11,39 +11,7 @@
 <div class="container">
 	<div class="columns">
 		<div class="column is-one-quarter">
-			<aside class="menu">
-				<p class="menu-label">Main Menu</p>
-				<ul class="menu-list">
-					<li><a class="is-active">Home</a></li>
-				</ul>
-				<p class="menu-label">My Account</p>
-				<ul class="menu-list">
-					<li><a>Settings</a></li>
-					<li><a>Favorites</a></li>
-					<li><a>Watch List</a></li>
-				</ul>
-				<p class="menu-label">Categories</p>
-				<ul class="menu-list">
-					<li><a>Agility</a></li>
-					<li><a>Cardio</a></li>
-					<li>
-						<ul>
-							<li><a>Extreme</a></li>
-							<li><a>Mixed Martial Arts</a></li>
-						</ul>
-					</li>
-					<li><a>Dance</a></li>
-					<li><a>Full-Body</a></li>
-					<li><a>Stretch</a></li>
-					<li>
-						<ul>
-							<li><a>Yoga</a></li>
-							<li><a>Pilates</a></li>
-						</ul>
-					</li>
-					<li><a>Weight Lifting</a></li>
-				</ul>
-			</aside>
+			<?php $this->view('account/sidebar'); ?>
 		</div>
 		<div class="column is three-quarters">
 			<div class="columns">
@@ -56,7 +24,7 @@
 						</div>
 					</div>
 				</div>
-				<div class="column is one-third">
+				<div class="column is one-third center">
 					<div class="box">
 						<div class="title is-4">Cardio</div>
 						<hr/>
@@ -65,7 +33,7 @@
 						</div>
 					</div>
 				</div>
-				<div class="column is one-third">
+				<div class="column is one-third center">
 					<div class="box">
 						<div class="title is-4">Dance</div>
 						<hr/>
@@ -76,7 +44,7 @@
 				</div>
 			</div>
 			<div class="columns">
-				<div class="column is one-third">
+				<div class="column is one-third center">
 					<div class="box">
 						<div class="title is-4">Full-Body</div>
 						<hr/>
@@ -85,7 +53,7 @@
 						</div>
 					</div>
 				</div>
-				<div class="column is one-third">
+				<div class="column is one-third center">
 					<div class="box">
 						<div class="title is-4">Stretch</div>
 						<hr/>
@@ -94,7 +62,7 @@
 						</div>
 					</div>
 				</div>
-				<div class="column is one-third">
+				<div class="column is one-third center">
 					<div class="box">
 						<div class="title is-4">Weight Lifting</div>
 						<hr/>
--- application/views/account/sidebar.php
+++ application/views/account/sidebar.php
@@ -1,13 +1,12 @@
 <aside class="menu">
-	<p class="menu-label">Main Menu</p>
-	<ul class="menu-list">
-		<li><a class="is-active">Home</a></li>
-	</ul>
 	<p class="menu-label">My Account</p>
 	<ul class="menu-list">
+		<li><a class="is-active">Home</a></li>
 		<li><a>Settings</a></li>
 		<li><a>Favorites</a></li>
-		<li><a>Watch List</a></li>
+		<li><a>Friends List</a></li>
+		<li><a>Achievements</a></li>
+		<li><a>Analytics</a></li>
 	</ul>
 	<p class="menu-label">Categories</p>
 	<ul class="menu-list">
@@ -15,7 +14,7 @@
 		<li><a>Cardio</a></li>
 		<li>
 			<ul>
-				<li><a>Extreme</a></li>
+				<li><a>Standard</a></li>
 				<li><a>Mixed Martial Arts</a></li>
 			</ul>
 		</li>
