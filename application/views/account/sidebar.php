8f7405ccd3d0b41d10944cc7c351896c2fd6c92c
= <chase.terry87@gmail.com>
2017-07-26 10:35:43 -0500

    Created a separate view for the user sidebar

---
+++ application/views/account/sidebar.php
@@ -0,0 +1,33 @@
+<aside class="menu">
+	<p class="menu-label">Main Menu</p>
+	<ul class="menu-list">
+		<li><a class="is-active">Home</a></li>
+	</ul>
+	<p class="menu-label">My Account</p>
+	<ul class="menu-list">
+		<li><a>Settings</a></li>
+		<li><a>Favorites</a></li>
+		<li><a>Watch List</a></li>
+	</ul>
+	<p class="menu-label">Categories</p>
+	<ul class="menu-list">
+		<li><a>Agility</a></li>
+		<li><a>Cardio</a></li>
+		<li>
+			<ul>
+				<li><a>Extreme</a></li>
+				<li><a>Mixed Martial Arts</a></li>
+			</ul>
+		</li>
+		<li><a>Dance</a></li>
+		<li><a>Full-Body</a></li>
+		<li><a>Stretch</a></li>
+		<li>
+			<ul>
+				<li><a>Yoga</a></li>
+				<li><a>Pilates</a></li>
+			</ul>
+		</li>
+		<li><a>Weight Lifting</a></li>
+	</ul>
+</aside>
